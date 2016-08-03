a923dc0cced75fc39f6e6fc26cbcda20b1e8d0f0
Douglas Drouillard <drouillard@gmail.com>
2016-08-03 16:20:15 -0400

    fix typo in heading

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React Materal Starter Kit</h2>
+          <h2>Welcome to Material React Starter Kit</h2>
         </div>
         <Grid fluid className="content">
           <Row center="xs">
